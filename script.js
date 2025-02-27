e0517457e57215d4b13719d0a6f77503dd9a3307
bsv011 <basavrajsirasgaonkar@gmail.com>
2025-02-27 12:35:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function myOnClickFn(){
+    document.location.href="LogIn_Form.html"
+}
+
+function myOnClickFn2(){
+    document.location.href="AboutUs.html"
+}
+
+function myOnClickFn3(){
+    document.location.href="ContactUs.html"
+}
+
+function myOnClickFn4(){
+    document.location.href="PlanningPlat.html"
+}
+function myRedirectFn(){
+    document.location.href="InPerson.html"
+}
+
+function myRedirectFn2(){
+    document.location.href="Virtual.html"
+}
+
+function myRedirectFn3(){
+    document.location.href="Hybrid.html"
+}
